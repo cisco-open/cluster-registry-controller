61d05049069d6e8846477bb537c02e0dd225ed1d
Peter Turi <turip@cisco.com>
2021-09-15 15:20:18 +0200

    Fix kubebuilder downloads

--- scripts/install_kubebuilder.sh
+++ scripts/install_kubebuilder.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2019 Banzai Cloud Zrt. All Rights Reserved.
+
 set -euo pipefail
 
-[ -z "${1:-}" ] && { echo "Usage: $0 <version>"; exit 1; }
+install=YES
+version=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+      -d|--download-only) install=NO; shift;;
+      *) version="$1"; shift ;;
+  esac
+done
 
-version=$1
+[ -z "${version}" ] && { echo "Usage: $0 [-d|--download-only] <version>"; exit 1; }
 
 target_dir_name=kubebuilder-${version}
 link_path=bin/kubebuilder
 
-[ -e ${link_path} ] && rm -r ${link_path}
-
 mkdir -p bin
-ln -s "${target_dir_name}" ${link_path}
+
+if [ $install = YES ]; then
+  [ -e ${link_path} ] && rm -r ${link_path}
+  ln -s "${target_dir_name}" ${link_path}
+fi
 
 if [ ! -e bin/"${target_dir_name}" ]; then
     os=$(go env GOOS)
     arch=$(go env GOARCH)
 
     # download kubebuilder and extract it to tmp
-    curl -L "https://go.kubebuilder.io/dl/${version}/${os}/${arch}" | tar -xz -C /tmp/
+    curl -L "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${version}_${os}_${arch}.tar.gz" | tar -xz -C /tmp
 
     # extract the archive
     mv "/tmp/kubebuilder_${version}_${os}_${arch}" bin/"${target_dir_name}"
