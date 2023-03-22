3b505b849c0139f1ec6d111de1e881b812d302bc
Tibor Kircsi <tkircsi@gmail.com>
2023-03-22 10:01:58 +0100

    bump kubebuilder-tools version to 1.26.1

--- scripts/install_kubebuilder_tools.sh
+++ scripts/install_kubebuilder_tools.sh
@@ -9,7 +9,7 @@ arch=$(go env GOARCH)
 
 # These are the current official versions for kubebuilder-tools (v1.19.2 or v1.22.x is recommended)
   # Reference: https://book.kubebuilder.io/reference/envtest.html#kubernetes-120-and-121-binary-issues
-verified_kubebuilder_tools_version=(1.19.2)
+verified_kubebuilder_tools_version=(1.26.1)
 
 
 FLOCK=`which flock 2> /dev/null || true`
@@ -42,6 +42,7 @@ fi
 
   if [[ ! -e "${target_dir}/complete" ]]; then
     echo "Installing kubebuilder-tools version v${kubebuilder_tools_version}..."
+    echo "Downloading https://go.kubebuilder.io/test-tools/${kubebuilder_tools_version}/${os}/${arch}"
     #  Validate kubebuilder-tools version
     if echo ${verified_kubebuilder_tools_version[@]} | grep -q -w ${kubebuilder_tools_version}; then
         curl -sSLo ${target_dir}/envtest-bins.tar.gz "https://go.kubebuilder.io/test-tools/${kubebuilder_tools_version}/${os}/${arch}"
